ec3b4fbf0d6e5183b7d8f64bfdbc284504c2c15d
nikitayashny <nikita.yashny@gmaile.com>
2025-02-17 14:49:10 +0300

    chat v0.0.1

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -7,6 +7,7 @@ import SockJS from "sockjs-client";
 import { findChatMessages, findChatMessage, countNewMessages } from "../http/chatAPI";
 import { fetchUserChats, fetchUsers } from "../http/userAPI";
 
+
 const Chat = observer(() => {
   const { user } = useContext(Context);
   const thisUserId = user.userId;
@@ -18,7 +19,7 @@ const Chat = observer(() => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [unreadMessagesCount, setUnreadMessagesCount] = useState({});
-
+  const [searchQuery, setSearchQuery] = useState("")
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
@@ -36,6 +37,7 @@ const Chat = observer(() => {
       setUsers(userList);
       userList.map((contact) => {
         countNewMessages(contact.id, thisUserId).then((count) => {  
+          console.log(contact.id + ": " + count)
           setUnreadMessagesCount((prev) => ({
             ...prev,
             [contact.id]: (prev[contact.id] || 0) + count,
@@ -145,19 +147,39 @@ const Chat = observer(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
+  const filteredUsers = users.filter(user => 
+    user.username.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <Container className="mt-5 mb-5" style={{ height: "80vh", background: "rgba(255,255,255,1)", borderRadius: "20px", padding: "20px" }}>
-      <div className="d-flex" style={{ height: "100%" }}>
+      <div className="d-flex" style={{ height: "6%" }}>
+        <div className="col-4" style={{ borderRight: "1px solid #ccc"}}>
+          <h4>Мессенджер</h4>
+        </div>
+        <div className="col-5">
+          <h4 className="ms-3">{selectedUser && selectedUser.username}</h4>
+        </div>
+      </div>
+      <div className="d-flex" style={{ height: "94%" }}>
         <div className="col-4" style={{ borderRight: "1px solid #ccc", padding: "10px", overflowY: "auto", height: "100%" }}>
+          <Form.Control
+            type="text"
+            placeholder="Поиск по имени..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            style={{ marginBottom: "10px" }}
+          />
           {loading ? (
             <Spinner animation="border" />
           ) : error ? (
             <Alert variant="danger">{error}</Alert>
           ) : (
             <ListGroup className="mb-3">
-              {users.map((user) => (
+              {filteredUsers.map((user) => (
                 user.id !== thisUserId ? (
                   <ListGroup.Item
+                    variant="light"
                     key={user.id}
                     action
                     active={selectedUser && selectedUser.id === user.id}
@@ -171,44 +193,50 @@ const Chat = observer(() => {
           )}
         </div>
 
-        <div className="col-8" style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+        <div className="col-8" style={{ height: "100%", display: "flex", flexDirection: "column", padding: "0 0 0 15px" }}>
           {selectedUser ? (
             <>
               <div style={{ flex: 1, overflowY: "auto" }}>
               <ListGroup>
-                  {messages
-                    .slice()
-                    .sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp))
-                    .map((msg, index) => {
-                      const messageDate = new Date(msg.timestamp);
-                      const today = new Date();
-                      const isToday = messageDate.toDateString() === today.toDateString();
+                {messages
+                  .slice()
+                  .sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp))
+                  .filter(msg => msg.senderId === selectedUser.id || msg.senderId === thisUserId)
+                  .map((msg, index) => {
+                    const messageDate = new Date(msg.timestamp);
+                    const today = new Date();
+                    const isToday = messageDate.toDateString() === today.toDateString();
+                    const currentYear = today.getFullYear();
+                    const messageYear = messageDate.getFullYear();
+                    const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
-                      return (
-                        <ListGroup.Item
-                          key={index}
-                          style={{
-                            display: "inline-block",
-                            maxWidth: "70%",
-                            textAlign: msg.senderId === thisUserId ? "right" : "left",
-                            backgroundColor: msg.senderId === thisUserId ? "#e1f5fe" : "#ffffff",
-                            border: "1px solid #ccc",
-                            borderRadius: "10px",
-                            padding: "10px",
-                            margin: "5px 0",
-                            alignSelf: msg.senderId === thisUserId ? "flex-end" : "flex-start"
-                          }}
-                        >
-                          <strong>{user.userName != msg.senderName && msg.senderName  + ":"} </strong>{msg.content}
-                          <div style={{ textAlign: 'right', fontSize: '0.8em', color: '#888' }}>
-                            {isToday 
-                              ? messageDate.toLocaleTimeString() 
-                              : messageDate.toLocaleDateString() + ' ' + messageDate.toLocaleTimeString()}
-                          </div>
-                        </ListGroup.Item>
-                      );
-                    })}
-                </ListGroup>
+                    return (
+                      <ListGroup.Item
+                        key={index}
+                        style={{
+                          display: "inline-block",
+                          maxWidth: "70%",
+                          textAlign: msg.senderId === thisUserId ? "right" : "left",
+                          backgroundColor: msg.senderId === thisUserId ? "#e1f5fe" : "#ffffff",
+                          border: "1px solid #ccc",
+                          borderRadius: "10px",
+                          padding: "10px",
+                          margin: "5px 0",
+                          alignSelf: msg.senderId === thisUserId ? "flex-end" : "flex-start"
+                        }}
+                      >
+                        {msg.content}
+                        <div style={{ textAlign: 'right', fontSize: '0.8em', color: '#888' }}>
+                          {isToday 
+                            ? messageDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) 
+                            : messageYear === currentYear
+                              ? messageDate.toLocaleDateString([], { month: 'long', day: 'numeric' }) + ' ' + messageDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+                              : messageDate.toLocaleDateString([], options) + ' ' + messageDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                        </div>
+                      </ListGroup.Item>
+                    );
+                  })}
+              </ListGroup>
                 <div ref={messagesEndRef} />
               </div>
               <Form style={{ display: "flex", alignItems: "center" }}>
@@ -218,6 +246,12 @@ const Chat = observer(() => {
                     placeholder="Введите сообщение..."
                     value={newMessage}
                     onChange={(e) => setNewMessage(e.target.value)}
+                    onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        e.preventDefault(); 
+                        sendMessage();
+                      }
+                    }}
                   />
                 </Form.Group>
                 <Button variant="primary" onClick={sendMessage} disabled={!selectedUser || newMessage.trim() === ""}>
